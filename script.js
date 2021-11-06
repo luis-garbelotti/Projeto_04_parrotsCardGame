70bfc366f47efb8ab7e67e2479dc3999048811ad
Luis Garbelotti <luisgsgarbelotti@gmail.com>
2021-11-06 12:12:58 -0300

    Adicionando e testando responsividade

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+let numeroDeCartas = 0;
+let todasAsCartas = [];
+
 function inicioDeJogo(){
 
-let numeroDeCartas = parseInt(prompt("Com quantas cartas quer jogar? \nObs: Escolha um numero par de cartas."));
+numeroDeCartas = parseInt(prompt("Com quantas cartas quer jogar? \nObs: Escolha um numero par de cartas entre 4 e 14."));
 
-if (numeroDeCartas % 2 !== 0) {
+if (numeroDeCartas % 2 !== 0 || numeroDeCartas < 4 || numeroDeCartas > 14) {
     inicioDeJogo();
 } else {
-    
+    sortearCartas();
 }
 } inicioDeJogo();
+
+
+function sortearCartas () {
+    // todasAsCartas.length = numeroDeCartas;
+
+    for (let i = 0; i < numeroDeCartas; i++) {
+        todasAsCartas.push(i);
+    }
+}
